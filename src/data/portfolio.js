faa14fe05a9af264abb4193a7f91dccff2c09718
Dvega0xE <dvega.001.dv@gmail.com>
2022-11-17 17:41:11 -0800

    Initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+	{
+		title: 'Disposable Task Force',
+		imgUrl: '/assets/DTF.png',
+		stack: ['Unity', 'C#', 'Play Here'],
+		link: 'https://traitor-jones.github.io/Disposable-Task-Force-Web/',
+	},
+	{
+		title: 'Resume',
+		imgUrl: '/assets/Resume.png',
+		stack: ['For More Projects'],
+		link: 'https://drive.google.com/file/d/15pn_uWt9qPlK3PDiCaSxMnbkmPGuA5z3/view?usp=sharing',
+	},
+	{
+		title: 'AC Mock-Up Unit',
+		imgUrl: '/assets/AC-unit.png',
+		stack: ['Arduino', 'C++', 'Embedded Systems'],
+		link: 'https://youtu.be/sYaNuOirehc',
+	},
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,37 @@
+export default [
+	{
+		year: 'Future',
+		title: 'Your Company?',
+		duration: 'X years',
+		details:
+			'You could help write and be a part of my journey as an Engineer.',
+	},
+	{
+		year: 'Today',
+		title: 'A Curious Engineer',
+		duration: 'X years',
+		details:
+			"While seeking employment, I take time to practice and strengthen my programming skills and critical analysis. Some of my recent curiosity has led me into Cyber Security and Cloud Architecture, but I still remain in love with backend development with C++.",
+	},
+	{
+		year: '2022',
+		title: "Bachelor's Degree in Computer Science",
+		duration: '2.5 years',
+		details:
+			'I pursued the knowledge that a computer science degree had to offer at UCR. I learned about computer architecture, theory of automata, data structures and algorithms, AI/ML, and some introduction to game development.',
+	},
+	{
+		year: '2020',
+		title: "Associates's Degree in Computer Science",
+		duration: '2.5 years',
+		details:
+			'During this time I had the opportunity to really immerse in the sciences. At the time not only did I work on my C++ foundation but also fell in love with physics and mathematics. I also had the opportunity to participate in NASA programs during this time.',
+	},
+	{
+		year: '2017',
+		title: 'Enrolled at Cerritos Community College',
+		duration: 'the beginning',
+		details:
+			"I decided to pursue a degree in Computer Science after working I.T. for a few years. In my time of employment I learned a lot about networks and hardware, but quickly found out that I lack knowledge in scripting and the software side. This led to pursue grow in tech.",
+	},
+]
