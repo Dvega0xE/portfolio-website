4a94e55c2e4deedf7cc24b2b32b27857087f56e2
Diego Vega <37664358+Dvega0xE@users.noreply.github.com>
2025-03-28 14:59:00 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,7 +57,7 @@ function Footer(){
 					</div>
 				</div>
 			<p className="text-sm mt-2  opacity-50">
-				&copy; {new Date().getFullYear()} Forrest Knight & Diego Vega. All rights reserved.
+				&copy; {new Date().getFullYear()} Diego Vega. All rights reserved.
 			</p>
 		</div>
 	);
